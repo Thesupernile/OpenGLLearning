87c250a148cf81e318df054e4845dea74f4fd1e9
Thesupernile <thesupernile6@gmail.com>
2026-02-25 20:00:13 +0000

    Linked up to openGL 4 and set up simple vertex buffer for drawing the same triangle as before

--- OpenGLLearning/src/application.cpp
+++ OpenGLLearning/src/application.cpp
@@ -1,40 +1,55 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
+    // Initialize glfw 
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    // Create a windowed mode window and its OpenGL context 
+    window = glfwCreateWindow(640, 480, "Program", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
+    // Make the openGL context
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    // Include the openGL functions using GLEW
+    if (glewInit() != GLEW_OK) {
+        return -1;
+    }
+
+    float vertexPositions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    // Create a vertex buffer
+    unsigned int bufferId;
+    glGenBuffers(1, &bufferId);
+    glBindBuffer(GL_ARRAY_BUFFER, bufferId);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), &vertexPositions, GL_STATIC_DRAW);
+
+    // Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+        // Render here 
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f( 0.0f,  0.5f);
-        glVertex2f( 0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        /* Swap front and back buffers */
+        // Swap front and back buffers
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        // Poll for and process events
         glfwPollEvents();
     }
 
